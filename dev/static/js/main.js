2259805bb46f04a49cce4d86261176d57c926373
Artur Ballod <artur.ballod@gmail.com>
2021-09-27 01:50:25 +0200

    #itemcard

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -7,5 +7,17 @@ if (catOpener) {
     catOpener.addEventListener('click', function () {
         navCat.classList.toggle('catalog-opened');
         catalogBody.classList.toggle('opened');
+        catOpener.classList.toggle('active');
     });
 };
+
+var upperItem = document.getElementsByClassName('menu__item');
+var elNodes = document.querySelectorAll(".menu__item");
+
+for (var i = 0; i < upperItem.length; i++) {
+    var elem = upperItem[i];
+    elem.addEventListener("click", function() {
+        event.preventDefault();
+        this.classList.toggle("pressed");
+    });
+}
