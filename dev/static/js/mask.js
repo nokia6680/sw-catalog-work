27da9f1a0f55fb93aee537cbfb6e82f46527cc00
Artur Ballod <artur.ballod@gmail.com>
2021-12-08 15:00:15 +0200

    213213

--- dev/static/js/mask.js
+++ dev/static/js/mask.js
@@ -1,50 +1,66 @@
-const input = document.getElementById("tel");
+"use strict";
 
-const prefixNumber = (str) => {
-  if (str === "7") {
-    return "7 (";
-  }
-  if (str === "8") {
-    return "8 (";
-  }
-  if (str === "9") {
-    return "7 (9";
-  }
-  return "7 (";
-};
+var input = document.getElementById("tel");
 
-// ======================================
-input.addEventListener("input", (e) => {
-  const value = input.value.replace(/\D+/g, "");
-  const numberLength = 11;
-
-  let result;
-  if (input.value.includes("+8") || input.value[0] === "8") {
-    result = "";
-  } else {
-    result = "+";
-  }
-
-  //
-  for (let i = 0; i < value.length && i < numberLength; i++) {
-    switch (i) {
-      case 0:
-        result += prefixNumber(value[i]);
-        continue;
-      case 4:
-        result += ") ";
-        break;
-      case 7:
-        result += "-";
-        break;
-      case 9:
-        result += "-";
-        break;
-      default:
-        break;
+var prefixNumber = function prefixNumber(str) {
+    if (str === "7") {
+        return "7 (";
     }
-    result += value[i];
-  }
-  //
-  input.value = result;
+
+    if (str === "8") {
+        return "7 (";
+    }
+
+    if (str === "9") {
+        return "7 (9";
+    }
+
+    return "7 (";
+}; // ======================================
+
+
+input.addEventListener("input", function(e) {
+    var value = input.value.replace(/\D+/g, "");
+    var numberLength = 11;
+    var result;
+
+    if (input.value == '+7 ' || input.value == '') {
+        input.value = '';
+        return false;
+    }
+
+    if (input.value.includes("+8") || input.value[0] === "8") {
+        result = "+";
+    } else {
+        result = "+";
+    } //
+
+
+    for (var i = 0; i < value.length && i < numberLength; i++) {
+        switch (i) {
+            case 0:
+                result += prefixNumber(value[i]);
+                continue;
+
+            case 4:
+                result += ") ";
+                break;
+
+            case 7:
+                result += "-";
+                break;
+
+            case 9:
+                result += "-";
+                break;
+
+            default:
+                break;
+        }
+
+        result += value[i];
+    } //
+
+
+    input.value = result;
 });
